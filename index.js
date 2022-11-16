4d1cec1b11fb356a3a730286d851d504a1223f95
Robert Pliszka <robplisz@gmail.com>
2022-11-15 20:20:31 -0500

    Commented code in index.js

--- index.js
+++ index.js
@@ -27,24 +27,29 @@ robot.numOfSensors = 250;
 
 console.log(robot.numOfSensors);
 
-
+// Creating a new user object
 const users = {
+  // Private properties
   _firstName: "Robert",
   _lastName: "Pliszka",
   _age: 35,
+  // getter which retrives all user information
   get userInfo() {
     return `User: ${this._firstName} ${this._lastName} Age: ${this._age}`;
   },
+  // setter for first name
   set newUserFirstName(firstName) {
     if (typeof firstName === "string") {
       this._firstName = firstName;
     }
   },
+  // settter for last name
   set newUserLastName(lastName) {
     if (typeof lastName === "string") {
       this._lastName = lastName;
     }
   },
+  // setter for age
   set newUserAge(age) {
     if (typeof age === "number") {
       this._age = age;
@@ -54,6 +59,7 @@ const users = {
 
 console.log(users.userInfo);
 
+// Assigning new values to object properties
 users.newUserFirstName = "Heidi";
 users.newUserLastName = "Done";
 users.newUserAge = 32;
