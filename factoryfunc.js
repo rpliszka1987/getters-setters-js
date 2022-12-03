0336578386c38b2ac1d090cd1047bb16e54f24c9
Robert Pliszka <robplisz@gmail.com>
2022-12-03 08:25:27 -0500

    Created a createCar factory function

--- factoryfunc.js
+++ factoryfunc.js
@@ -13,3 +13,21 @@ const createCharacter = (name, level) => {
 const spiderman = createCharacter("Spider-Man", 100);
 
 spiderman.hello();
+
+
+const createCar = (year, make, model) => {
+    return {
+        year: year,
+        make: make,
+        model: model,
+        carInfo() {
+            return(
+                `This is a ${this.year} ${this.make} ${this.model}. For more information contact dealer.`
+            )
+        }
+    }
+}
+
+const toyotaRav = createCar(2023, 'Toyota', 'Rav4');
+
+console.log(toyotaRav.carInfo())
