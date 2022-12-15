fd401f4053a3c0c526b7d37b51054c948ce6b8bf
Robert Pliszka <robplisz@gmail.com>
2022-12-15 13:45:24 -0500

    Added comments to teamStats.js

--- teamStats.js
+++ teamStats.js
@@ -1,6 +1,8 @@
 // This is a Team Stats challenge from Code Academy Solution. Description in README.md file.
 
+// Creating a team obj
 const team = {
+  // players obj array
   _players: [
     {
       firstName: "Robert",
@@ -18,6 +20,7 @@ const team = {
       age: 34,
     },
   ],
+  //   gmaes obj array
   _games: [
     {
       opponent: "Poland",
@@ -35,12 +38,15 @@ const team = {
       opponentPoints: 1,
     },
   ],
+  //   player getter
   get players() {
     return this._players;
   },
+  //   games getter
   get games() {
     return this._games;
   },
+  //   add new player method
   addPlayer(newFirstName, newLastName, newAge) {
     this.players.push({
       firstName: newFirstName,
@@ -48,6 +54,7 @@ const team = {
       age: newAge,
     });
   },
+  //   add new game method
   addGame(newOpponents, newTeamPoints, newOpponentPoints) {
     this._games.push({
       opponent: newOpponents,
@@ -57,8 +64,12 @@ const team = {
   },
 };
 
-team.addPlayer("Johnny", "Walker", 54)
-team.addGame("Dominican Republic", 5, 3)
+// creating new player
+team.addPlayer("Johnny", "Walker", 54);
 
-console.log(team.players)
-console.log(team.games)
+// creating new game
+team.addGame("Dominican Republic", 5, 3);
+
+// retriving information from team obj
+console.log(team.players);
+console.log(team.games);
